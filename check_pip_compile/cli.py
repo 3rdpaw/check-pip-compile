94584827afbb1c83de50008926a0afd2457b2765
Michael Butt <3rdpaw@users.noreply.github.com>
2021-07-07 12:09:09 -0400

    Add output file option (#1)
    * chore: upgrade to click 8.x
    
    * chore: test adding option
    
    * chore: bump version
    
    remove unused code now that output file is required
    
    Co-authored-by: Michael Butt <michael.butt@cyware.com>

--- check_pip_compile/cli.py
+++ check_pip_compile/cli.py
@@ -17,35 +17,20 @@ import check_pip_compile
 @click.version_option(version=check_pip_compile.__version__)
 @click.command()
 @click.argument(
-    "paths", type=click.Path(exists=True, file_okay=True, dir_okay=True), nargs=-1
+    "paths", type=click.Path(exists=True, file_okay=True, dir_okay=False), nargs=-1
 )
+@click.option('-o', '--output-file', required=True)
 def entry_point(paths):
     """Check if pip-compile needs to be run."""
     succeeded = True
     for in_file in paths:
-        if os.path.isdir(in_file):
-            tmp = check_directory(in_file)
-        else:
-            txt_file: Optional[str] = get_corresponding_txt_file(in_file)
-            if not txt_file or not os.path.isfile(txt_file):
-                txt_file = None
-            tmp = check_file(in_file, txt_file)
+        txt_file: Optional[str] = output_file
+        if not txt_file or not os.path.isfile(txt_file):
+            txt_file = None
+        tmp = check_file(in_file, txt_file)
         succeeded = succeeded and tmp
     if not succeeded:
         sys.exit(-1)
-
-
-def check_directory(path: str) -> bool:
-    in_files = discover_in_files(path)
-    txt_files = discover_txt_files(path)
-    matched_files = match_in_with_txt(in_files, txt_files)
-
-    succeeded = True
-    for in_file, txt_file in matched_files:
-        tmp = check_file(in_file, txt_file)
-        succeeded = tmp and succeeded
-    return succeeded
-
 
 def check_file(in_file: Optional[str], txt_file: Optional[str]) -> bool:
     """Return True, if it's ok."""
@@ -66,38 +51,9 @@ def check_file(in_file: Optional[str], txt_file: Optional[str]) -> bool:
     return succeeded
 
 
-def discover_in_files(path: str) -> List[str]:
-    p = Path(path)
-    in_files = [str(el) for el in p.glob("*.in")]
-    if os.path.isfile(p / "setup.py"):
-        in_files.append(str(p / "setup.py"))
-    return in_files
-
-
-def discover_txt_files(path: str) -> List[str]:
-    p = Path(path)
-    return [str(el) for el in p.glob("*.txt")]
-
-
-def get_corresponding_txt_file(in_file: str) -> str:
+def get_corresponding_txt_file(in_file: str, compiled_file: str) -> str:
     if in_file.endswith("setup.py"):
         txt_file = in_file[: -len("setup.py")] + "requirements.txt"
     else:
-        txt_file = in_file[:-2] + "txt"
+        txt_file = compiled_file
     return txt_file
-
-
-def match_in_with_txt(
-    in_files: List[str], txt_files: List[str]
-) -> List[Tuple[Optional[str], Optional[str]]]:
-    matched_files: List[Tuple[Optional[str], Optional[str]]] = []
-    for in_file in in_files:
-        txt_file = get_corresponding_txt_file(in_file)
-        if txt_file in txt_files:
-            matched_files.append((in_file, txt_file))
-            txt_files.remove(txt_file)
-        else:
-            matched_files.append((in_file, None))
-    for txt_file in txt_files:
-        matched_files.append((None, txt_file))
-    return matched_files
